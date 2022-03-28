da2ad3c8eda2ca814ca3e99a3861fb352ad6b82c
lukascobit <lukyn.odehnal333@gmail.com>
2022-03-28 14:51:22 +0200

    sign in

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,67 +1,82 @@
 import React, { NewLifecycle, useState } from "react";
 
 function login() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [errorCode, setErrorCode] = useState("");
+	const serverDomain = process.env.NEXT_PUBLIC_SERVERDOMAIN;
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [errorCode, setErrorCode] = useState("");
 
-    function submit(e: any) {
-        localStorage.setItem("email", email);
-        e.preventDefault();
-        if (!email && !password) {
-            setErrorCode("Email and password is mandatory");
-            return;
-        } else if (email.length >= 3000) {
-            setErrorCode("Zpráva je moc dlouhá");
-            return;
-        }
+	function submit(e: any) {
+		localStorage.setItem("email", email);
+		e.preventDefault();
+		if (!email && !password) {
+			setErrorCode("Email and password is mandatory");
+			return;
+		} else if (email.length >= 300) {
+			setErrorCode("The email is too long");
+			return;
+		}
 
-        (async function () {
-            const Rbody = { body };
-            const Rusername = { username };
-            const Rgenre = { genre };
+		(async function () {
+			const Remail: any = { email };
+			const Rpassword = { password };
 
-            localStorage.setItem("username", username);
+			localStorage.setItem("email", email);
 
-            const arr = [Rbody, Rusername, Rgenre];
-            const response = await fetch(`${serverDomain}drby`, {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(arr),
-            });
-            window.location.href = "/";
-        })();
-    }
+			const response = await fetch(
+				`${serverDomain}users-login/${Remail.email}`,
+				{
+					method: "POST",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify([Rpassword]),
+				}
+			);
+			const jsonData = await response.json();
+			console.log(jsonData);
+			if (!jsonData.length) {
+				setErrorCode("Wrong email and password");
+				return;
+			}
+			const data = jsonData[0];
+			console.log(data);
 
-    return (
-        <div className="content">
-            <h1>Log in to your existing account</h1>
-            <form className="signUpForm" action="/">
-                <label htmlFor="email">Email</label>
-                <input
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    placeholder="your@email.com"
-                    type="email"
-                    id="email"
-                />
+			localStorage.setItem("username", data.username);
+			localStorage.setItem("email", data.email);
+			localStorage.setItem("pfp", data.pfp);
 
-                <label htmlFor="password">Password</label>
-                <input
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    type="password"
-                    id="password"
-                />
+			window.location.href = "/";
+		})();
+	}
 
-                <h1 className="error">{errorCode}</h1>
+	return (
+		<div className="content">
+			<h1>Log in to your existing account</h1>
+			<form className="signUpForm" action="/">
+				<label htmlFor="email">Email</label>
+				<input
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					placeholder="your@email.com"
+					type="email"
+					id="email"
+				/>
 
-                <button onClick={submit} className="smallButton">
-                    Sign up
-                </button>
-            </form>
-        </div>
-    );
+				<label htmlFor="password">Password</label>
+				<input
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					type="password"
+					id="password"
+				/>
+
+				<h1 className="error">{errorCode}</h1>
+
+				<button onClick={submit} className="smallButton">
+					Log in
+				</button>
+			</form>
+		</div>
+	);
 }
 
 export default login;
