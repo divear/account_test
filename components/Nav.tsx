49ae5ce435b6bd897f63094e1912636693f771a0
lukascobit <lukyn.odehnal333@gmail.com>
2022-04-22 16:00:17 +0200

    favicon

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -13,6 +13,7 @@ function Nav() {
 	const [imgLink, setImgLink] = useState(
 		"https://firebasestorage.googleapis.com/v0/b/accounts-8a8bf.appspot.com/o/pfp%2Ftwitter-default-pfp.jpeg?alt=media"
 	);
+	const [width, setWidth] = useState(0);
 
 	useEffect(() => {
 		setHasAccount(!!localStorage.getItem("id"));
@@ -26,6 +27,7 @@ function Nav() {
 					: localStorage.getItem("pfp")
 			}`
 		);
+		setWidth(window.innerWidth);
 	}, []);
 
 	console.log(modal);
@@ -35,8 +37,6 @@ function Nav() {
 			<div className="wave">
 				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
 					<path
-						width={100}
-						height={100}
 						fill="#f7be89"
 						fillOpacity="1"
 						d="M0,224L48,197.3C96,171,192,117,288,96C384,75,480,85,576,96C672,107,768,117,864,133.3C960,149,1056,171,1152,170.7C1248,171,1344,149,1392,138.7L1440,128L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
