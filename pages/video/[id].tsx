06541ad2f9499747407d555eb9496a8255ad91d0
lukascobit <lukyn.odehnal333@gmail.com>
2022-05-05 22:43:43 +0200

    video player

--- pages/video/[id].tsx
+++ pages/video/[id].tsx
@@ -1,15 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import ReactPlayer from 'react-player'
 
 function Video() {
-  const router = useRouter()
+	const [data, setData] = useState<any>([]);
+  const {query} = useRouter()
+  const id = query.id
+  console.log(id);
+  
+
+	const serverDomain =
+		process.env.NODE_ENV === "development"
+			? "http://localhost:4000/"
+			: process.env.NEXT_PUBLIC_SERVERDOMAIN;
+
+	useEffect(() => {
+    console.log(query.id);
+  
+    const id = query.id
+    console.log(id);
+		async function getBlog() {
+			try {
+        if(!id){
+          return
+        }
+				const response = await fetch(`${serverDomain}posts/${id}`);
+				const jsonData = await response.json();
+				setData(jsonData[0]);
+        console.log(jsonData[0]);
+        
+			} catch (error) {
+				console.log(error);
+			}
+		}
+		getBlog();
+	}, [id]);
+  
 
   return (
     <div className='content'>
+      <h1>{data.body}</h1>
       <ReactPlayer 
-      url='
-      https://firebasestorage.googleapis.com/v0/b/accounts-8a8bf.appspot.com/o/video%2Fbad_pc_build.mp4?alt=media&token=43da284d-9301-4137-b444-5e921f6debce' 
+      url={data.video_id}
       controls />
     </div>
   )
