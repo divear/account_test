78f437b4dbf071fa9ccbb83431952d7356d346c1
lukascobit <lukyn.odehnal333@gmail.com>
2022-06-04 00:15:12 +0200

    time ago (doesnt work)

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,6 +27,55 @@ const Home: NextPage = () => {
 		setHasAccount(!!localStorage.getItem("id"));
 	}, []);
 
+	function timeAgo(time: any) {
+		time = +new Date(time.toLocaleString());
+		var time_formats = [
+			[60, "seconds", 1], // 60
+			[120, "1 minute ago", "1 minute from now"], // 60*2
+			[3600, "minutes", 60], // 60*60, 60
+			[7200, "1 hour ago", "1 hour from now"], // 60*60*2
+			[86400, "hours", 3600], // 60*60*24, 60*60
+			[172800, "Yesterday", "Tomorrow"], // 60*60*24*2
+			[604800, "days", 86400], // 60*60*24*7, 60*60*24
+			[1209600, "Last week", "Next week"], // 60*60*24*7*4*2
+			[2419200, "weeks", 604800], // 60*60*24*7*4, 60*60*24*7
+			[4838400, "Last month", "Next month"], // 60*60*24*7*4*2
+			[29030400, "months", 2419200], // 60*60*24*7*4*12, 60*60*24*7*4
+			[58060800, "Last year", "Next year"], // 60*60*24*7*4*12*2
+			[2903040000, "years", 29030400], // 60*60*24*7*4*12*100, 60*60*24*7*4*12
+			[5806080000, "Last century", "Next century"], // 60*60*24*7*4*12*100*2
+			[58060800000, "centuries", 2903040000], // 60*60*24*7*4*12*100*20, 60*60*24*7*4*12*100
+		];
+		var seconds = (+new Date() - time) / 1000,
+			token = "ago",
+			list_choice = 1;
+
+		if (seconds == 0) {
+			return "Just now";
+		}
+		if (seconds < 0) {
+			seconds = Math.abs(seconds);
+			token = "from now";
+			list_choice = 2;
+		}
+		var i = 0,
+			format;
+		while ((format = time_formats[i++]))
+			if (seconds < format[0]) {
+				if (typeof format[2] == "string") return format[list_choice];
+				else
+					return (
+						Math.floor(seconds / format[2]) +
+						" " +
+						format[1] +
+						" " +
+						token
+					);
+			}
+		return time;
+	}
+	console.log(timeAgo("01/04/2022 15:20"));
+
 	return (
 		<div className="content ">
 			<div className={postModal ? "postModal" : "hidePostModal"}>
@@ -59,7 +108,9 @@ const Home: NextPage = () => {
 								<i className="username" title={d.email}>
 									{d.username}
 								</i>
-								<h6 className="date">{d.posted_date}</h6>
+								<h6 className="date">
+									{timeAgo(d.posted_date)}
+								</h6>
 								<span className="email">{d.email}</span>
 							</div>
 							<h2>{d.body}</h2>
--- pages/video/[id].tsx
+++ pages/video/[id].tsx
@@ -1,13 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
-import ReactPlayer from 'react-player'
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import ReactPlayer from "react-player";
 
 function Video() {
 	const [data, setData] = useState<any>([]);
-  const {query} = useRouter()
-  const id = query.id
-  console.log(id);
-  
+	const { query } = useRouter();
+	const id = query.id;
 
 	const serverDomain =
 		process.env.NODE_ENV === "development"
@@ -15,39 +13,34 @@ function Video() {
 			: process.env.NEXT_PUBLIC_SERVERDOMAIN;
 
 	useEffect(() => {
-    console.log(query.id);
-  
-    const id = query.id
-    console.log(id);
+		const id = query.id;
 		async function getBlog() {
 			try {
-        if(!id){
-          return
-        }
+				if (!id) {
+					return;
+				}
 				const response = await fetch(`${serverDomain}posts/${id}`);
 				const jsonData = await response.json();
 				setData(jsonData[0]);
-        console.log(jsonData[0]);
-        
 			} catch (error) {
 				console.log(error);
 			}
 		}
 		getBlog();
 	}, [id]);
-  
 
-  return (
-    <div className='content'>
-      <ReactPlayer 
-      className="video"
-      width={960}
-      height={540}
-      url={data.video_id}
-      controls />
-      <h1>{data.body}</h1>
-    </div>
-  )
+	return (
+		<div className="content">
+			<ReactPlayer
+				className="video"
+				width={960}
+				height={540}
+				url={data.video_id}
+				controls
+			/>
+			<h1>{data.body}</h1>
+		</div>
+	);
 }
 
 export default Video;
